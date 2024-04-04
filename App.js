4ab6fee3edb33285bc156bdc2a13075b12a4e258
DESKTOP-6TDH965\astas <astas2286@gmail.com>
2024-04-04 15:35:21 +0200

    no message

--- App.js
+++ App.js
@@ -8,8 +8,8 @@ export default function App() {
         <TextInput style={styles.textInput} placeholder='Your course goal' />
         <Button title='Add goal' />
       </View>
-      <View>
-        <Text>List of goals</Text>
+      <View style={styles.goalsContainer}>
+        <Text>List of goals...</Text>
       </View>
     </View>
   );
@@ -17,17 +17,28 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+    backgroundColor:'#e8f4ff'
   },
   inputContainer: {
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBlockEndColor: '#cccccc'
   },
   textInput: {
     borderWidth: 1,
     borderColor: '#cccccc',
-    width: '80%',
+    width: '70%',
     marginRight: 8,
     padding: 8
+  },
+  goalsContainer: {
+    flex: 4
   }
 });
